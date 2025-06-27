f366834140c37130e5207e84fb1ed72f12d0ae5f
yesh89 <99314219+yesh89@users.noreply.github.com>
2025-06-27 14:23:53 +0530

    Commiting New Changes

--- Proj-QSP_Java/src/com/inheritance/RunSingleInheritance.java
+++ Proj-QSP_Java/src/com/inheritance/RunSingleInheritance.java
@@ -20,10 +20,13 @@ public class RunSingleInheritance {
 	public static void main(String[] args) {
 		A1 objA = new A1();
 		objA.m1();
+		System.out.println(objA.i);
 		System.out.println("*********************");
 		B1 objB = new B1();
 		objB.m2();
 		objB.m1();
+		System.out.println(objB.i);
+		System.out.println(objB.j);
 
 	}
 }
